ec11b94b045165a872346049e8bdbc28f8a7abfc
RAOUL Florence <r19000243@V-PJ-47-031.salsa.univ-amu.fr>
2022-01-20 09:50:22 +0100

    :sparkles: add main +show solution

--- puissanceN.c
+++ puissanceN.c
@@ -6,6 +6,27 @@
 #include <string.h>
 #include <assert.h>
 
+list_t openList_p, closedList_p;
+
+void showSolution( Item *goal )
+{
+  int i = 0, j;
+
+  printf("\nSolution:");
+
+  while (goal)
+  {
+    printBoard( goal );
+    goal = goal->parent;
+    i++;
+  }
+
+  printf("\nLength of the solution = %d\n", i-1);
+  printf("Size of open list = %d\n", openList_p.numElements);
+  printf("Size of closed list = %d\n", closedList_p.numElements);
+}
+
+
 float minimax(Item* node, int depth, int player)
 {
   float value;
@@ -37,11 +58,38 @@ float minimax(Item* node, int depth, int player)
 
     }
 }
-//appel initial minimax(origin, depth, TRUE)
 
+
+//appel initial minimax(origin, depth, TRUE)
+void jeu(){
+  Item *cur_node, *child_p, *temp, *board;
+  int i;
+  board=initBoard()
+
+
+}
 
 int main(int argc, char const *argv[])
 {
+  initList(&openList_p);
+  initList(&closedList_p);
 
+
+  printf("\nInitial:");
+  Item *initial_state = initGame();
+  printBoard( initial_state );
+
+  /*int choix;
+  printf("\nChoix de la taille du puissanceN  :\n ");
+  scanf("%d", &choix);
+  //ALIGN_PAWN =choix;*/
+
+
+  //on continue la prtie tant que evaluateBoard est false // True = victoire
+  //while evaluateBoard() == false
+
+  /* clean lists */
+  cleanupList( &openList_p );
+  cleanupList( &closedList_p );
     return 0;
 }
